479b5a110d060aff2e420ca098919b022917db11
Jamal Seyedmohammadi <44075279+jamal94sm@users.noreply.github.com>
2024-09-03 19:49:32 +0330

    fedk2p.sh

--- fedk2p.sh
+++ fedk2p.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #SBATCH --account=def-arashmoh
 #SBATCH --gpus-per-node=1          # no. of gpu nodes
-#SBATCH --mem=32000M               # memory per node (ex: 16G) you can get more 
+#SBATCH --mem=16000M               # memory per node (ex: 16G) you can get more 
 #SBATCH --time=0-2:00 		   # time period you need for your code (it is 12 hours for example)
 #SBATCH --mail-user=<jamal73sm@gmail.com> 	# replace with your email address to get emails to know when it is started or failed. 
 #SBATCH --mail-type=ALL
@@ -11,4 +11,4 @@ module purge
 module load python
 source ~/FeDK2P/bin/activate  	# activate your environment
 
-python fedk2p.py   	# this is the direction and the name of your code
+python FeDK2P/fedk2p.py   	# this is the direction and the name of your code
